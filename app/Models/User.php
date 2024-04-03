df207149067983230fad77ffd26b362185dcd5c9
ratrimanik <ratrimanik2404@gmail.com>
2024-04-03 10:02:50 +0700

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'first_name',
-        'last_name',
+        'firstname',
+        'lastname',
         'email',
         'password',
     ];
